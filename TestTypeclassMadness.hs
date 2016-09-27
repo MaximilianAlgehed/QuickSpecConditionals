0fd5dfc8c025d4d4735c9e6246341178817707aa
Maximilian Algehed <m.algehed@gmail.com>
2016-09-27 15:27:13 +0200

    For some strange reason QuickSpec freezes but each component works fine when tested outside of QS

--- TestTypeclassMadness.hs
+++ TestTypeclassMadness.hs
@@ -6,21 +6,14 @@ import Data.Dynamic
 sig =
   signature {
     maxTermSize = Just 10,
-    instances = [names (NamesFor ["p"] :: NamesFor Predicates),
-                 baseType (undefined :: Predicates)
-                ],
     constants = [
        constant "[]" ([] :: [Int]),
        constant ":"  ((:) :: Int -> [Int] -> [Int]),
        constant "++" ((++) :: [Int] -> [Int] -> [Int]),
-       constant "head" (head :: [Int] -> Int),
-       constant "acc0" ((\(P preds) -> fromJust $ fromDynamic (preds `at` 0) :: [Int]) :: Predicates -> [Int])
-    ]-- ++(snd (preds prds))
+       constant "head" (head :: [Int] -> Int)
+    ]
    }
 
 prds = [predicate ((not . null) :: [Int] -> Bool)]
 
-instance Arbitrary Predicates where
-    arbitrary = fst $ preds prds
-
-main = quickSpec sig
+main = quickSpec $ predicateSig sig prds
