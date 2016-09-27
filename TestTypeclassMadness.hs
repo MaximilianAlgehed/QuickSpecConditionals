68ed9893d3e57fc65477941ee216af0cb91afecb
Maximilian Algehed <m.algehed@gmail.com>
2016-09-27 18:36:23 +0200

    Blerehrehrasd

--- TestTypeclassMadness.hs
+++ TestTypeclassMadness.hs
@@ -20,6 +20,8 @@ prds = [predicate ((not . null) :: [Int] -> Bool)]
 instance Arbitrary Predicates where
     -- This is broken in QuickSpec, but running "ghci> fmap (extract 0) Test.QuickCheck.generate arbitrary :: IO [Int]" runs fine
     -- and generates an actual value, what is the problem here?!
+    -- if we run the hand-written code that has been commented out (which sadly is not an option if we want generality)
+    -- it runs fine in QuickSpec! what the heck...
     arbitrary = fst $ preds prds {-do
                     a <- arbitrary `suchThat` (not . null) :: Gen [Int]
                     return $ P [(1, [toDyn a])]
